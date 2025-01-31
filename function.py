9edc01a8f4b65b677283aba95f8dfe4cbce5eec9
sonianavaei <sonianavaei@gmail.com>
2025-01-31 10:00:18 +0330

    first commit

---
+++ function.py
@@ -0,0 +1,12 @@
+
+from pprint import pprint
+def get_contact():
+    name=input("please enter a name : ")
+    phonneNumber=input("please enter phone number")
+    contact_dict = {"name": name, "phone_number":phonneNumber}
+    
+    return contact_dict
+
+
+print(get_contact())
+
